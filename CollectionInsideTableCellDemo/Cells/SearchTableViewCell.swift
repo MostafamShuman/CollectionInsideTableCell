fd0680614a689d44bb77472f566f142eb520637a
mostafa shuman <mostafa.shuman@hotmail.com>
2018-10-26 15:27:13 +0200

    first commit

---
+++ CollectionInsideTableCellDemo/Cells/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  CollectionInsideTableCellDemo
+//
+//  Created by Mostafa Mohamed on 10/26/18.
+//  Copyright © 2018 Mostafa Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CollectionInsideTableCellDemo/Model/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  CollectionInsideTableCellDemo
+//
+//  Created by Mostafa Mohamed on 10/26/18.
+//  Copyright © 2018 Mostafa Mohamed. All rights reserved.
+//
+
+import Foundation
